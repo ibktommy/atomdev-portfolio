7b766c9fe9535ca0b6e0fa876cc2076e56fab9ea
ibktommy <atomquartz@gmail.com>
2022-01-17 12:04:48 +0100

    Initial commit of my Personal Portfolio

---
+++ app.js
@@ -0,0 +1,58 @@
+// SERVICES OVERLAY
+
+const servicesDesignBtn = document.querySelector(
+    ".services-design .services-btn"
+);
+const servicesDevBtn = document.querySelector(".services-dev .services-btn");
+const servicesDev = document.querySelector(".services-modal_dev");
+const servicesDesign = document.querySelector(".services-modal_design");
+const designClosebtn = document.querySelector(".design-close");
+const devClosebtn = document.querySelector(".dev-close");
+
+servicesDesignBtn.addEventListener("click", openOverlayDesign);
+servicesDevBtn.addEventListener("click", openOverlayDev);
+designClosebtn.addEventListener("click", (e) => {
+    e.target.parentElement.classList.remove("show");
+});
+devClosebtn.addEventListener("click", (e) => {
+    e.target.parentElement.classList.remove("show");
+});
+
+function openOverlayDesign() {
+    servicesDesign.classList.add("show");
+}
+function openOverlayDev() {
+    servicesDev.classList.add("show");
+}
+
+// NAVIGATION MENU
+
+const navMenu = document.querySelector(".nav-menu");
+const navLines = document.querySelector(".line");
+const navlinks = document.querySelector("#navlinks");
+
+navMenu.addEventListener("click", () => {
+    navMenu.classList.toggle("change");
+    navlinks.classList.toggle("change");
+});
+
+// DARK MODE
+
+const html = document.documentElement;
+const home = document.querySelector("#home");
+console.log(home);
+const displayMenu = document.querySelector(".display-menu");
+const moonIcon = document.querySelector(".moon-icon");
+displayMenu.addEventListener("click", () => {
+    displayMenu.classList.toggle("change");
+
+    if (displayMenu.className.includes("change")) {
+        html.setAttribute("data-theme", "dark");
+        home.classList.remove("default-bg");
+        home.classList.add("new-bg");
+    } else {
+        html.setAttribute("data-theme", "light");
+        home.classList.remove("new-bg");
+        home.classList.add("default-bg");
+    }
+});
