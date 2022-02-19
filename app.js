bebb0b87b3bf79e8b57482731f63477b085b6a18
ibktommy <atomquartz@gmail.com>
2022-02-19 15:58:38 +0100

    Update Form Submission Function

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 // SERVICES OVERLAY
 
-const servicesDesignBtn = document.querySelector(
-    ".services-design .services-btn"
-);
+const servicesDesignBtn = document.querySelector(".services-design .services-btn");
 const servicesDevBtn = document.querySelector(".services-dev .services-btn");
 const servicesDev = document.querySelector(".services-modal_dev");
 const servicesDesign = document.querySelector(".services-modal_design");
@@ -12,17 +10,17 @@ const devClosebtn = document.querySelector(".dev-close");
 servicesDesignBtn.addEventListener("click", openOverlayDesign);
 servicesDevBtn.addEventListener("click", openOverlayDev);
 designClosebtn.addEventListener("click", (e) => {
-    e.target.parentElement.classList.remove("show");
+	e.target.parentElement.classList.remove("show");
 });
 devClosebtn.addEventListener("click", (e) => {
-    e.target.parentElement.classList.remove("show");
+	e.target.parentElement.classList.remove("show");
 });
 
 function openOverlayDesign() {
-    servicesDesign.classList.add("show");
+	servicesDesign.classList.add("show");
 }
 function openOverlayDev() {
-    servicesDev.classList.add("show");
+	servicesDev.classList.add("show");
 }
 
 // NAVIGATION MENU
@@ -32,8 +30,8 @@ const navLines = document.querySelector(".line");
 const navlinks = document.querySelector("#navlinks");
 
 navMenu.addEventListener("click", () => {
-    navMenu.classList.toggle("change");
-    navlinks.classList.toggle("change");
+	navMenu.classList.toggle("change");
+	navlinks.classList.toggle("change");
 });
 
 // DARK MODE
@@ -43,31 +41,68 @@ const home = document.querySelector("#home");
 const displayMenu = document.querySelector(".display-menu");
 const moonIcon = document.querySelector(".moon-icon");
 displayMenu.addEventListener("click", () => {
-    displayMenu.classList.toggle("change");
+	displayMenu.classList.toggle("change");
 
-    if (displayMenu.className.includes("change")) {
-        html.setAttribute("data-theme", "dark");
-        home.classList.remove("default-bg");
-        home.classList.add("new-bg");
-    } else {
-        html.setAttribute("data-theme", "light");
-        home.classList.remove("new-bg");
-        home.classList.add("default-bg");
-    }
-});
-
-// FORM SUBMIT EVENT
-document.querySelector("#contact form").addEventListener("submit", (e) => {
-    e.preventDefault();
+	if (displayMenu.className.includes("change")) {
+		html.setAttribute("data-theme", "dark");
+		home.classList.remove("default-bg");
+		home.classList.add("new-bg");
+	} else {
+		html.setAttribute("data-theme", "light");
+		home.classList.remove("new-bg");
+		home.classList.add("default-bg");
+	}
 });
 
 // SHOW TOP BACKGROUND
 const aboutSection = document.querySelector("#about");
 window.addEventListener("scroll", () => {
-    if (
-        window.pageYOffset + window.innerHeight >=
-        aboutSection.offsetHeight / 2
-    ) {
-        document.querySelector(".top-bg").classList.add("change");
-    }
+	if (window.pageYOffset + window.innerHeight >= aboutSection.offsetHeight / 2) {
+		document.querySelector(".top-bg").classList.add("change");
+	}
 });
+
+// name="simple-contact-form" accept-charset="utf-8" action="https://formspree.io/f/xjvlwony"
+//                 method="post"
+
+// FORM SUBMIT EVENT
+const form = document.querySelector(".contact-form");
+
+form.addEventListener("submit", function (e) {
+	e.preventDefault();
+
+	sendData();
+	form.reset();
+	console.log(e);
+});
+
+function sendData() {
+	let userName = document.querySelector(".user-name").value,
+		userEmail = document.querySelector(".user-email").value,
+		userMessage = document.querySelector(".user-message").value;
+
+	if (userName === "" || userEmail === "" || userMessage === "") {
+		alert("PLEASE FILL ALL FORM FIELDS!");
+	} else {
+		axios({
+			url: "https://formspree.io/f/xjvlwony",
+			method: "post",
+			headers: {
+				Accept: "application/json",
+			},
+			data: {
+				name: userName,
+				email: userEmail,
+				message: userMessage,
+			},
+		})
+			.then((response) => {
+				if (response.status === 200) {
+					alert("THANKS, YOUR MESSAGE HAS BEEN SENT");
+				}
+			})
+			.catch((error) => {
+				alert("OOPS! SOMETHING WENT WRONG");
+			});
+	}
+}
